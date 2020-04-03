df46b87e82623e6a1be5f617c2df085d3fbfbae4
Saurabh Kumar <saurabh.cse04@gmail.com>
2020-04-03 15:28:04 +0530

    setting style dynamically

--- src/App.js
+++ src/App.js
@@ -44,7 +44,8 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroungColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
@@ -65,6 +66,7 @@ class App extends Component {
           })}
         </div>
       )
+      style.backgroundColor = 'red';
     }
     return (
       <div className="App">
